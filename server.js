a1ecf5aaf54de0d755dc43d6591a88f17daa3493
TuhinPramanik4 <tuhinp475@gmail.com>
2025-02-12 19:42:28 +0530

    Create profile Route

--- server.js
+++ server.js
@@ -1,17 +1,27 @@
-import express from 'express'
-import path from 'path'
-const app = express()
+import express from 'express';
+import path from 'path';
+import bodyParser from 'body-parser';
 
-app.use('/Public', express.static('/Public'));
+const app = express();
 
 
-app.set("view engine","ejs");
-app.set("views",path.join(process.cwd(),"views"));
+app.use('/Public', express.static(path.join(process.cwd(), 'Public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/",(req,res)=>{
+app.set("view engine", "ejs");
+app.set("views", path.join(process.cwd(), "views"));
+
+// Route for Landing Page
+app.get("/", (req, res) => {
     res.render("LandingPage");
-})
+});
 
-app.listen(8000,()=>{
-    console.log("Server Started");
-})
+// Route for Profile Page
+app.get("/profile", (req, res) => {
+    res.render('Profile');
+});
+
+app.listen(8000, () => {
+    console.log("Server started on http://localhost:8000");
+});
