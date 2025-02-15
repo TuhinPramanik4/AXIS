e0e8cd65b64f59847b4241cdb48ca0ab9891618c
TUHIN BISWAS <116175379+Tuhinbiswas90@users.noreply.github.com>
2025-02-15 12:28:01 +0530

    Add files via upload

---
+++ yoga/sctipt.js
@@ -0,0 +1,8 @@
+window.addEventListener('scroll',function(){
+    let navbar = document.querySelector('.navbar');
+    if(this.window.scrolly > 20){
+        navbar.classList.add('scrolled')
+    }else{
+        navbar.classList.remove('scrolled')
+    }
+})
